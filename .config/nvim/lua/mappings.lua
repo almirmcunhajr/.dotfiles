abdc755519de566b8fc562d4b2683ab0b7a736db
Almir Menezes da Cunha Júnior <almirmdacunha@gmail.com>
2026-05-11 13:25:20 -0300

    Add p10k.zsh and nvim config

---
+++ .config/nvim/lua/mappings.lua
@@ -0,0 +1,62 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+local gitsigns = require('gitsigns')
+
+map('n', ']c', function()
+  if vim.wo.diff then
+    vim.cmd.normal({']c', bang = true})
+  else
+    gitsigns.nav_hunk('next')
+  end
+end)
+
+map('n', '[c', function()
+  if vim.wo.diff then
+    vim.cmd.normal({'[c', bang = true})
+  else
+    gitsigns.nav_hunk('prev')
+  end
+end)
+
+map('n', '<leader>hs', gitsigns.stage_hunk)
+map('n', '<leader>hr', gitsigns.reset_hunk)
+
+map('v', '<leader>hs', function()
+  gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+end)
+
+map('v', '<leader>hr', function()
+  gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+end)
+
+map('n', '<leader>hS', gitsigns.stage_buffer)
+map('n', '<leader>hR', gitsigns.reset_buffer)
+map('n', '<leader>hp', gitsigns.preview_hunk)
+map('n', '<leader>hi', gitsigns.preview_hunk_inline)
+
+map('n', '<leader>hb', function()
+  gitsigns.blame_line({ full = true })
+end)
+
+map('n', '<leader>hd', gitsigns.diffthis)
+
+map('n', '<leader>hD', function()
+  gitsigns.diffthis('~')
+end)
+
+map('n', '<leader>hQ', function() gitsigns.setqflist('all') end)
+map('n', '<leader>hq', gitsigns.setqflist)
+
+map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
+map('n', '<leader>tw', gitsigns.toggle_word_diff)
+
+map({'o', 'x'}, 'ih', gitsigns.select_hunk)
